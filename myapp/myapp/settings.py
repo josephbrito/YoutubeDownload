d32364b76d7ae481b79876d85140915c026d0469
josephbrito <jokesta02@gmail.com>
2024-05-13 16:25:56 -0300

    whitenoise

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -49,8 +48,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = 'myapp.urls'
 
@@ -118,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'youtube/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- myapp/myapp/wsgi.py
+++ myapp/myapp/wsgi.py
@@ -7,10 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
-from whitenoise import WhiteNoise
-
-
-
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -18,10 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myapp.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root="youtube/static")
-
-application.add_files("youtube/static/assets/*", prefix="more-files-assets/")
-application.add_files("youtube/static/css/*", prefix="more-files-css/")
-application.add_files("youtube/static/js/*", prefix="more-files-js/")
 
 app = application
